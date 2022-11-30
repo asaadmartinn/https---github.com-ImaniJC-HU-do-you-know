c89b150846c51b8c53de9eea278dee66a0818cc8
asaadmartinn <asaad.martin@bison.howard.edu>
2022-11-30 14:36:11 -0500

    Commiting what I have so far

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{textAlign: "center", marginBottom: 10 }} >
+        Made with ❤️ by Asaad, Mariah, Imani, Leandra, Obi
+    </div>
+  );
+};
+
+export default Footer
